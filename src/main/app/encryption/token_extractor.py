d28e74048e0ab741f554696a2248719d58cc9192
arcibashev71 <arcibashev71@yandex.ru>
2024-03-17 14:57:25 +0500

    test (token_extractor): добавить простые тесты

--- src/main/app/encryption/token_extractor.py
+++ src/main/app/encryption/token_extractor.py
@@ -11,7 +11,7 @@ class TokenType(Enum):
 
 
 class TokenExtractor:
-    def extract_tokens_from_string(self, string):
+    def extract_tokens_from_string(self, string) -> list[Token]:
         """
         extract by camelCase, snake_case, terminator and non alphanum symbols
         :param string: string
@@ -33,34 +33,34 @@ class TokenExtractor:
             tokens.append(Token(current_lexeme, symbol_type))
         return tokens
 
-    def _is_new_lexeme(self, prev_symbol_type, symbol, symbol_type):
+    def _is_new_lexeme(self, prev_symbol_type, symbol, symbol_type) -> bool:
         return prev_symbol_type and (
                 symbol_type != prev_symbol_type or
                 self._is_upper_letter(symbol) or
                 self._is_non_alphanum(symbol)
         )
 
-    def _determinate_type(self, symbol: bytes):
+    def _determinate_type(self, symbol: bytes) -> TokenType:
         if self._is_letter(symbol):
             return TokenType.LETTERS
         if self._is_digit(symbol):
             return TokenType.DIGITS
         return TokenType.OTHER
 
-    def _is_non_alphanum(self, symbol):
+    def _is_non_alphanum(self, symbol) -> bool:
         return not (self._is_letter(symbol) or self._is_digit(symbol))
 
     @staticmethod
-    def _is_letter(symbol):
+    def _is_letter(symbol) -> bool:
         return (
                 ord(b'a') <= symbol <= ord(b'z')
                 or ord(b'A') <= symbol <= ord(b'Z')
         )
 
     @staticmethod
-    def _is_upper_letter(symbol):
+    def _is_upper_letter(symbol) -> bool:
         return ord(b'A') <= symbol <= ord(b'Z')
 
     @staticmethod
-    def _is_digit(symbol):
+    def _is_digit(symbol) -> bool:
         return ord(b'0') <= symbol <= ord(b'9')
--- src/test/test_token_extractor.py
+++ src/test/test_token_extractor.py
@@ -1,23 +1,169 @@
 import unittest
 
-from src.main.app.encryption.token_extractor import TokenExtractor
+from src.main.app.encryption.token_extractor import TokenExtractor, Token, TokenType
 
 
 class Test(unittest.TestCase):
-    def test_simple(self):
-        string = b'firstSecond_third#four_FiveSix__seven=eight123_45%^&nine'
-        self._print(TokenExtractor().extract_tokens_from_string(string))
+    def test_simplest_letters(self):
+        string = b'first'
+        expected = self._make_tokens_by_mapping_from_str_values([Token('first', TokenType.LETTERS)])
+        actual = TokenExtractor().extract_tokens_from_string(string)
+        self.assertEqual(actual, expected)
 
-    def test_string_with_space(self):
-        string = b'first second  third\nfour\r\nfive'
-        self._print(TokenExtractor().extract_tokens_from_string(string))
+    def test_simplest_digits(self):
+        string = b'1234'
+        expected = self._make_tokens_by_mapping_from_str_values([Token('1234', TokenType.DIGITS)])
+        actual = TokenExtractor().extract_tokens_from_string(string)
+        self.assertEqual(actual, expected)
+
+    def test_simplest_other(self):
+        string = b'*!$'
+        expected = self._make_tokens_by_mapping_from_str_values(
+            [
+                Token('*', TokenType.OTHER),
+                Token('!', TokenType.OTHER),
+                Token('$', TokenType.OTHER)
+            ]
+        )
+        actual = TokenExtractor().extract_tokens_from_string(string)
+        self.assertEqual(actual, expected)
+
+    def test_camel_case(self):
+        string = b'firstSecondThird'
+        expected = self._make_tokens_by_mapping_from_str_values(
+            [
+                Token('first', TokenType.LETTERS),
+                Token('Second', TokenType.LETTERS),
+                Token('Third', TokenType.LETTERS)
+            ]
+        )
+        actual = TokenExtractor().extract_tokens_from_string(string)
+        self.assertEqual(actual, expected)
+
+    def test_snake_case(self):
+        string = b'first_second_Third'
+        expected = self._make_tokens_by_mapping_from_str_values(
+            [
+                Token('first', TokenType.LETTERS),
+                Token('_', TokenType.OTHER),
+                Token('second', TokenType.LETTERS),
+                Token('_', TokenType.OTHER),
+                Token('Third', TokenType.LETTERS)
+            ]
+        )
+        actual = TokenExtractor().extract_tokens_from_string(string)
+        self.assertEqual(actual, expected)
+
+    def test_string_with_spaces1(self):
+        string = b'first second  3 '
+        expected = self._make_tokens_by_mapping_from_str_values(
+            [
+                Token('first', TokenType.LETTERS),
+                Token(' ', TokenType.OTHER),
+                Token('second', TokenType.LETTERS),
+                Token(' ', TokenType.OTHER),
+                Token(' ', TokenType.OTHER),
+                Token('3', TokenType.DIGITS),
+                Token(' ', TokenType.OTHER)
+            ]
+        )
+        actual = TokenExtractor().extract_tokens_from_string(string)
+        self.assertEqual(actual, expected)
+
+    def test_string_with_spaces2(self):
+        string = b'first\nsecond'
+        expected = self._make_tokens_by_mapping_from_str_values(
+            [
+                Token('first', TokenType.LETTERS),
+                Token('\n', TokenType.OTHER),
+                Token('second', TokenType.LETTERS)
+            ]
+        )
+        actual = TokenExtractor().extract_tokens_from_string(string)
+        self.assertEqual(actual, expected)
+
+    def test_string_with_spaces3(self):
+        string = b'\r\nfirst second \n'
+        expected = self._make_tokens_by_mapping_from_str_values(
+            [
+                Token('\r', TokenType.OTHER),
+                Token('\n', TokenType.OTHER),
+                Token('first', TokenType.LETTERS),
+                Token(' ', TokenType.OTHER),
+                Token('second', TokenType.LETTERS),
+                Token(' ', TokenType.OTHER),
+                Token('\n', TokenType.OTHER)
+            ]
+        )
+        actual = TokenExtractor().extract_tokens_from_string(string)
+        self.assertEqual(actual, expected)
+
+    def test_simple1(self):
+        string = b'four+5=9'
+        expected = self._make_tokens_by_mapping_from_str_values(
+            [
+                Token('four', TokenType.LETTERS),
+                Token('+', TokenType.OTHER),
+                Token('5', TokenType.DIGITS),
+                Token('=', TokenType.OTHER),
+                Token('9', TokenType.DIGITS)
+            ]
+        )
+        actual = TokenExtractor().extract_tokens_from_string(string)
+        self.assertEqual(actual, expected)
+
+    def test_simple2(self):
+        string = b'TwentyOne-6 == 15!'
+        expected = self._make_tokens_by_mapping_from_str_values(
+            [
+                Token('Twenty', TokenType.LETTERS),
+                Token('One', TokenType.LETTERS),
+                Token('-', TokenType.OTHER),
+                Token('6', TokenType.DIGITS),
+                Token(' ', TokenType.OTHER),
+                Token('=', TokenType.OTHER),
+                Token('=', TokenType.OTHER),
+                Token(' ', TokenType.OTHER),
+                Token('15', TokenType.DIGITS),
+                Token('!', TokenType.OTHER)
+            ]
+        )
+        actual = TokenExtractor().extract_tokens_from_string(string)
+        self.assertEqual(actual, expected)
+
+    def test_php_variable_assignment(self):
+        string = b'$num = 10;'
+        expected = self._make_tokens_by_mapping_from_str_values(
+            [
+                Token('$', TokenType.OTHER),
+                Token('num', TokenType.LETTERS),
+                Token(' ', TokenType.OTHER),
+                Token('=', TokenType.OTHER),
+                Token(' ', TokenType.OTHER),
+                Token('10', TokenType.DIGITS),
+                Token(';', TokenType.OTHER)
+            ]
+        )
+        actual = TokenExtractor().extract_tokens_from_string(string)
+        self.assertEqual(actual, expected)
+
+    def _make_tokens_by_mapping_from_str_values(self, tokens: list[Token[str, TokenType]]) \
+            -> list[Token[list[int], TokenType]]:
+        return list(map(self._map_str_val_to_numbers, tokens))
 
     @staticmethod
-    def _print(tokens):
+    def _map_str_val_to_numbers(token: Token[str, TokenType]) -> Token[list[int], TokenType]:
+        new_val = list(map(ord, token.value))
+        return Token(new_val, token.type)
+
+    def _print(self, tokens: list[Token]) -> None:
         print()
         for token in tokens:
-            val_as_str = ''.join(tuple(map(chr, token.value)))
-            print(f'{repr(val_as_str)} -> {token.type}')
+            print(f'{repr(self._convert_numbers_of_bytes_to_str(token.value))} -> {token.type}')
+
+    @staticmethod
+    def _convert_numbers_of_bytes_to_str(numbers: list[int]) -> str:
+        return ''.join(tuple(map(chr, numbers)))
 
 
 if __name__ == '__main__':
