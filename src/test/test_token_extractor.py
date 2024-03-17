9691bff1c78abf4d258f9d031b1a7f6ba528df77
arcibashev71 <arcibashev71@yandex.ru>
2024-03-17 13:39:46 +0500

    test: тест для TokenExtractor, проверяющий пробелы и спецсимволы

--- src/test/test_token_extractor.py
+++ src/test/test_token_extractor.py
@@ -6,10 +6,18 @@ from src.main.app.encryption.token_extractor import TokenExtractor
 class Test(unittest.TestCase):
     def test_simple(self):
         string = b'firstSecond_third#four_FiveSix__seven=eight123_45%^&nine'
+        self._print(TokenExtractor().extract_tokens_from_string(string))
+
+    def test_string_with_space(self):
+        string = b'first second  third\nfour\r\nfive'
+        self._print(TokenExtractor().extract_tokens_from_string(string))
+
+    @staticmethod
+    def _print(tokens):
         print()
-        for token in TokenExtractor().extract_tokens_from_string(string):
+        for token in tokens:
             val_as_str = ''.join(tuple(map(chr, token.value)))
-            print(f'{val_as_str} -> {token.type}')
+            print(f'{repr(val_as_str)} -> {token.type}')
 
 
 if __name__ == '__main__':
