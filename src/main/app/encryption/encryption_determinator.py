6ad5168cef78f86a21e01e9135a4cd777300b0f5
arcibashev71 <arcibashev71@yandex.ru>
2024-03-23 14:11:06 +0500

    ref: encr_deter

--- src/main/app/encryption/encryption_determinator.py
+++ src/main/app/encryption/encryption_determinator.py
@@ -2,12 +2,20 @@ class EncryptionDeterminator:
     def __init__(self, entropy_analyzer):
         self._entropy_analyzer = entropy_analyzer
         self._window_encryption_border = 60
+        self._unconditional_lower_bound_of_entropy = 70
+        self._conditional_lower_bound_of_entropy = 65
+        self._percent_of_entropy_vals_for_window = 55
 
     def determinate(self, data):
         entropy = self._entropy_analyzer.analyze(data)
         entropies, window_size, hop = self._entropy_analyzer.window_analyze(data)
         count_above_border = len(list(filter(lambda entr: entr >= self._window_encryption_border, entropies)))
         entropy_above_border = round(100 * count_above_border / len(entropies))
-        if entropy >= 70 or entropy >= 65 and entropy_above_border >= 55:
-            return True, entropy, entropy_above_border
-        return False, entropy, entropy_above_border
+        return self._is_encr(entropy, entropy_above_border), entropy, entropy_above_border
+
+    def _is_encr(self, entropy, entropy_above_border):
+        return (
+                entropy >= self._unconditional_lower_bound_of_entropy
+                or entropy >= self._conditional_lower_bound_of_entropy
+                and entropy_above_border >= self._percent_of_entropy_vals_for_window
+        )
