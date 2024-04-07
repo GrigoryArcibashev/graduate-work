3fcbcef499e0c3277806585694a02de36c818016
arcibashev71 <arcibashev71@yandex.ru>
2024-04-07 20:57:06 +0500

    ref: stat_maker.py

--- src/main/app/encryption/stat_maker.py
+++ src/main/app/encryption/stat_maker.py
@@ -1,3 +1,5 @@
+from os import listdir
+from os.path import isfile, join
 from src.main.app.encryption.encr_filter.encryption_filter import EncryptionFilter
 from src.main.app.encryption.encr_filter.words.word_loader import SimpleWordLoader
 from src.main.app.encryption.encr_filter.words.word_provider import WordProvider
@@ -12,50 +14,78 @@ def make_stat():
 
     ed = EncryptionDeterminator(EntropyAnalyzer(Entropy()))
     ef = EncryptionFilter(WordProvider(SimpleWordLoader('encr_filter/words/words.txt')))
-    prefixes = ['../../source/orig/', '../../source/encr/']
-    postfix = '.txt'
-    files = [str(i) for i in range(1, 21)]
 
-    with open('../../source/encr_stat.txt', 'w') as file_stat:
-        for prefix in prefixes:
-            for filename in files:
-                text = list(read_file(prefix + filename + postfix))
-                len_before = len(text)
-                filtered_text = ef.filter(text)
-                cut_out = round(100 - len(filtered_text) / (len_before / 100), 2)
-                is_encr, entropy, entropy_above_border = ed.determinate(filtered_text)
-                if prefix == prefixes[0]:
-                    # шифра быть не должно
-                    if is_encr:
-                        fp += 1
-                    else:
-                        tn += 1
-                else:
-                    # шифр должен быть
-                    if is_encr:
-                        tp += 1
-                    else:
-                        fn += 1
+    encr_files, no_encr_files = get_files_for_stat()
 
-                file_stat.write(f'ИМЯ: {prefix + filename + postfix}\n')
-                file_stat.write(f'\tЭнтропия {entropy}% | {entropy_above_border}%\n')
-                file_stat.write(f'\tВырезано {cut_out}%\n')
-                file_stat.write(f'{">ЕСТЬ ШИФР" if is_encr else ">НЕТ ШИФРА"}\n\n')
-            file_stat.write('-' * 20 + '\n\n')
+    file_stat = open('../../source/encr_stat.txt', 'w')
+    for filename in no_encr_files:
+        cut_out, entropy, entropy_above_border, is_encr = calc_entropy(ed, ef, filename)
+        write_result(cut_out, entropy, entropy_above_border, file_stat, filename, is_encr)
+        if is_encr:
+            fp += 1
+        else:
+            tn += 1
+    for filename in encr_files:
+        cut_out, entropy, entropy_above_border, is_encr = calc_entropy(ed, ef, filename)
+        write_result(cut_out, entropy, entropy_above_border, file_stat, filename, is_encr)
+        if is_encr:
+            tp += 1
+        else:
+            fn += 1
+    precision, recall, f_score = calc_metrics(fn, fp, tp)
+    write_metrics(f_score, file_stat, fn, fp, precision, recall, tn, tp)
+    file_stat.close()
 
-        precision = tp / (tp + fp)
-        recall = tp / (tp + fn)
-        f_score = 2 * recall * precision / (recall + precision)
+    print(f'TP {tp}\nTN {tn}\nFP {fp}\nFN {fn}')
+    print(f'Точность = {round(100 * precision)}%')
+    print(f'Полнота = {round(100 * recall)}%')
+    print(f'F-score = {round(100 * f_score)}%')
 
-        file_stat.write(f'TP {tp}\nTN {tn}\nFP {fp}\nFN {fn}\n')
-        file_stat.write(f'Точность = {round(100 * precision)}%\n')
-        file_stat.write(f'Полнота = {round(100 * recall)}%\n')
-        file_stat.write(f'F-score = {round(100 * f_score)}%\n')
 
-        print(f'TP {tp}\nTN {tn}\nFP {fp}\nFN {fn}')
-        print(f'Точность = {round(100 * precision)}%')
-        print(f'Полнота = {round(100 * recall)}%')
-        print(f'F-score = {round(100 * f_score)}%')
+def get_files_for_stat():
+    path_to_encr = ['../../source/encr/', '../../source/encr/CSS/']
+    path_to_non_encr = ['../../source/orig/']
+    encr_files = get_filenames_by_path(path_to_encr)
+    no_encr_files = get_filenames_by_path(path_to_non_encr)
+    return encr_files, no_encr_files
+
+
+def get_filenames_by_path(paths):
+    filenames = list()
+    for path in paths:
+        filenames.extend(filter(isfile, (map(lambda f: join(path, f), listdir(path)))))
+    return filenames
+
+
+def write_metrics(f_score, file_stat, fn, fp, precision, recall, tn, tp):
+    file_stat.write(f'TP {tp}\nTN {tn}\nFP {fp}\nFN {fn}\n')
+    file_stat.write(f'Точность = {round(100 * precision)}%\n')
+    file_stat.write(f'Полнота = {round(100 * recall)}%\n')
+    file_stat.write(f'F-score = {round(100 * f_score)}%\n')
+
+
+def calc_metrics(fn, fp, tp):
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f_score = 2 * recall * precision / (recall + precision)
+    return precision, recall, f_score
+
+
+def calc_entropy(ed, ef, filename):
+    text = list(read_file(filename))
+    len_before = len(text)
+    filtered_text = ef.filter(text)
+    cut_out = round(100 - len(filtered_text) / (len_before / 100), 2)
+    is_encr, entropy, entropy_above_border = ed.determinate(filtered_text)
+    return cut_out, entropy, entropy_above_border, is_encr
+
+
+def write_result(cut_out, entropy, entropy_above_border, file_stat, filename, is_encr):
+    file_stat.write(f'ИМЯ: {filename}\n')
+    file_stat.write(f'\tЭнтропия {entropy}% | {entropy_above_border}%\n')
+    file_stat.write(f'\tВырезано {cut_out}%\n')
+    file_stat.write(f'{">ЕСТЬ ШИФР" if is_encr else ">НЕТ ШИФРА"}\n\n')
+    file_stat.write('-' * 20 + '\n\n')
 
 
 if __name__ == '__main__':
