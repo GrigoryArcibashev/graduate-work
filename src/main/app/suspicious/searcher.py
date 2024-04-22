084ac67f7aa35e44581d39f6e5d97396fd0e3b53
arcibashev71 <arcibashev71@yandex.ru>
2024-04-22 15:40:13 +0500

    feat: реализовать серчер подозрит. слов

--- src/main/app/suspicious/searcher.py
+++ src/main/app/suspicious/searcher.py
@@ -32,10 +32,10 @@ class SuspiciousType(Enum):
 
 
 class SuspiciousCode:
-    def __init__(self, code: bytes, code_type: SuspiciousType, danger_lvl: DangerLevel):
+    def __init__(self, code: bytes, danger_lvl: DangerLevel, code_type: SuspiciousType):
         self.__code = code
-        self.__type = code_type
         self.__lvl = danger_lvl
+        self.__type = code_type
 
     @property
     def code(self) -> bytes:
@@ -61,21 +61,35 @@ class SuspiciousCode:
         return f'{self.danger_lvl} => {self.type} => {self.code}'
 
 
+class PatternContainer:
+    def __init__(self, pattern: re.Pattern, danger_lvl: DangerLevel, code_type: SuspiciousType):
+        self.pattern = pattern
+        self.danger_lvl = danger_lvl
+        self.type = code_type
+
+    def __str__(self):
+        return f'{self.danger_lvl} => {self.type} => {self.pattern}'
+
+
 class SuspySearcher:
+
     def __init__(self):
         self._patterns = {
             DangerLevel.DANGEROUS: {
-                SuspiciousType.GENERAL: {},
-                SuspiciousType.COMMAND: {},
-                SuspiciousType.ENCRYPT: {},
-                SuspiciousType.EXECUTION: {},
-                SuspiciousType.FILES: {},
-                SuspiciousType.IMPORT: {},
-                SuspiciousType.NET: {},
-                SuspiciousType.OS: {},
+                SuspiciousType.EXECUTION: {
+                    re.compile(br'[^\w\'\"]([Ee]val)(?:\s+[\'\"\w]|\s*\()'),
+                    re.compile(br'\W(passthru)\s*\('),
+                    re.compile(br'\W(proc_open)\s*\('),
+                    re.compile(br'[^\w\'\"](system)(?:\s+[\'\"\w]|\s*\()'),
+                    re.compile(br'[^\w\'\"]((?:shell_|pcntl_)?exec(?:File|[vl]p?e?|)(?:Sync|))(?:\s+[\'\"\w]|\s*\()'),
+                    re.compile(br'\W(ICodeCompiler)\W'),
+                    re.compile(br'\W(CodeDom\.Compiler[.;])\W'),
+                    re.compile(br'\W(CodeAnalysis\.(?:CSharp\.)?Scripting[.;])\W'),
+                    re.compile(br'\W(CSharpScript\.(?:Run|Evaluate)Async)\W'),
+                    re.compile(br'\W(innerHTML)\s*\('),
+                },
             },
             DangerLevel.SUSPICIOUS: {
-                SuspiciousType.GENERAL: {},
                 SuspiciousType.COMMAND: {
                     re.compile(
                         br'\W([\"\'] *'
@@ -89,195 +103,128 @@ class SuspySearcher:
                         re.IGNORECASE
                     ),
                 },
-                SuspiciousType.ENCRYPT: {},
-                SuspiciousType.EXECUTION: {},
-                SuspiciousType.FILES: {},
-                SuspiciousType.IMPORT: {},
-                SuspiciousType.NET: {},
-                SuspiciousType.OS: {},
+                SuspiciousType.ENCRYPT: {
+                    re.compile(br'\W((?:standard_|urlsafe_)?[ab](?:16|32|64|85)(?:hex)?(?:en|de)code)\s*\('),
+                    re.compile(br'\W((?:strict_|urlsafe_)?(?:en|de)code64)\s*[({]'),
+                    re.compile(br'\W(gzinflate)\s*\('),
+                    re.compile(br'\W(base64_(?:en|de)code)\s*\('),
+                    re.compile(br'\W(convert_uu(?:en|de)code)\s*\('),
+                    re.compile(br'\W(str_rot13)\s*\('),
+                    re.compile(br'\W(atob)\s*\('),
+                    re.compile(br'\W(btoa)\s*\('),
+                    re.compile(br'\W((?:From|To)Base64(?:Transform|String|CharArray))\W'),
+                    re.compile(br'\W(SoapHexBinary)\W'),
+                },
+                SuspiciousType.FILES: {
+                    re.compile(br'\W(FileStream)\W'),
+                    re.compile(br'\W(FileMode\s*.\s*OpenOrCreate)\W'),
+                    re.compile(br'\W(DriveInfo)\W'),
+                    re.compile(br'\W(DirectoryInfo)\W'),
+                    re.compile(br'\W(Create(?:Subd|D)irectory|SymbolicLink)\s*\('),
+                    re.compile(br'\W(Enumerate(?:File(?:System(?:Infos)?|s)|Directories))\s*\('),
+                    re.compile(
+                        br'\W(Get(?:Drives|File(?:SystemEntries|s|Name|)|'
+                        + br'(?:Current)?Directory(?:Name)?|Directories))\s*\('
+                    ),
+                    re.compile(br'\W(Open(?:Read|Text|Write))\s*\('),
+                    re.compile(br'\W((?:Read|Write)All(?:Lines|Bytes|Text)(?:Async)?)\s*\('),
+                    re.compile(br'\W(ReadLines(?:Async)?)\s*\('),
+                    re.compile(br'\W((?:re|sys)open)\s*\('),
+                    re.compile(br'\W(each_line)\s*[({]'),
+                    re.compile(
+                        br'\Wf(?:ile)?\s*\.\s*((?:write|read)(?:lines?)?)',
+                        re.IGNORECASE
+                    ),
+                    re.compile(br'\W(directory\?)\s*\('),
+                    re.compile(br'\W((?:read|execut)able(?:_real)?\?)\s*\('),
+                    re.compile(br'\W([lf]?ch(?:grp|mod|own)(?:Sync|))\s*\('),
+                    re.compile(br'\W(file(?:_exists|_get_contents|_put_contents|inode|size|type|\?))\s*\('),
+                    re.compile(br'\W(f(?:cntl|open|passthru|read|scanf|[lr]?seek|(?:data)?sync|stat|write))\s*\('),
+                    re.compile(br'\W(dirname)\s*\('),
+                    re.compile(br'\W(is_?(?:dir|executable|file|link|readable|uploaded_file|write?able))\s*\('),
+                    re.compile(br'\W(mk(?:dirs?|node)(?:Sync|))\s*\('),
+                    re.compile(br'\W(move_uploaded_file)\s*\('),
+                    re.compile(br'\W(pathinfo)\s*\('),
+                    re.compile(br'\W(p(?:open|readv?|writev?|ipe))\s*\('),
+                    re.compile(br'\W(rm(?:dir|)(?:Sync|))\s*\('),
+                    re.compile(br'\W(appendFile(?:Sync|))\s*\('),
+                    re.compile(br'\W(create(?:Read|Write)Stream)\s*\('),
+                    re.compile(br'\.(fd)\W'),
+                    re.compile(br'\W(read(?:v|dir|[Ff]ile|link|[Ll]ines?|partial|ableWebStream)(?:Sync|))\s*\('),
+                    re.compile(br'\W(write(?:v|File)(?:Sync|))\s*\('),
+                    re.compile(br'\W(open(?:dir|AsBlob)(?:Sync|))\s*\('),
+                    re.compile(br'\W(removedirs)\s*\('),
+                    re.compile(
+                        br'\W(eio_(?:f?ch(?:mod|own)|fallocate|'
+                        + br'ftruncate|init|link|mk(?:dir|nod)|open|'
+                        + br'read(?:dir|link)?|realpath|rmdir|'
+                        + br'seek|sendfile|write))\s*\('
+                    ),
+                },
+                SuspiciousType.IMPORT: {
+                    re.compile(br'\W(include(?:_once)?\s*\(.+?\))'),
+                    re.compile(br'\W(require\s*\(\s*[\"\'](?:fs|multer|express-fileupload|socket\.io)[\"\']\s*\))'),
+                },
+                SuspiciousType.NET: {
+                    re.compile(br'\W(\$_FILES(?:\s*\[.+?])+)'),
+                    re.compile(br'\W(Download(?:Data|File|String)(?:(?:Task)?Async)?)\s*\('),
+                },
+                SuspiciousType.OS: {
+                    re.compile(br'\W(access)\s*\('),
+                    re.compile(br'\.(environb?)\W'),
+                    re.compile(br'\W(get(?:envb?|_exec_path))\s*\('),
+                    re.compile(br'\W(putenv)\s*\('),
+                },
             },
             DangerLevel.PAY_ATTENTION: {
-                SuspiciousType.GENERAL: {},
-                SuspiciousType.COMMAND: {},
-                SuspiciousType.ENCRYPT: {},
-                SuspiciousType.EXECUTION: {},
-                SuspiciousType.FILES: {},
-                SuspiciousType.IMPORT: {},
-                SuspiciousType.NET: {},
-                SuspiciousType.OS: {},
+                SuspiciousType.GENERAL: {
+                    re.compile(br'\W([\"\']/bin/sh[\"\'])\W', re.IGNORECASE),
+                    re.compile(br'((?:web)?shell(?:exec)?)', re.IGNORECASE),
+                    re.compile(br'[_\W](base_?(?:16|32|64|85))', re.IGNORECASE),
+                    re.compile(br'[_\W](rot_?13)', re.IGNORECASE),
+                },
+                SuspiciousType.EXECUTION: {
+                    re.compile(br'\W(\$_GET\s*\[.+?])'),
+                    re.compile(br'\W(\$_REQUEST\s*\[.+?])'),
+                    re.compile(br'\W((?:popen|capture)(?:3|2e?))\s*\('),
+                },
+                SuspiciousType.FILES: {
+                    re.compile(br'(with\s+open\(.+?\)\s+as.+?:)'),
+                    re.compile(br'(=\s*open\(.+\))'),
+                },
+                SuspiciousType.IMPORT: {
+                    re.compile(br'\W(require(?:_once)?\s*\(.+?\))'),
+                },
+                SuspiciousType.NET: {
+                    re.compile(br'\W(urllib\.urlretrieve)\s*\('),
+                    re.compile(br'\W(requests\.get)\s*\('),
+                    re.compile(br'\W(Net\s*::\s*HTTP\s*\.\s*get(?:_response)?\s*\(.+\))'),
+                    re.compile(br'\W(http\s*\.\s*request(?:_get)?\s*\(.+\))'),
+                },
             },
         }
 
-    def search(self, text: bytes) -> tuple[SuspiciousCode]:
+    def search(self, text: bytes) -> list[SuspiciousCode]:
         searched: set[SuspiciousCode] = set()
-        for pattern, lvl, _type in self._get_next_pattern():
-            found = set(pattern.findall(text))
+        for pc in self._get_next_pattern():
+            found = set(pc.pattern.findall(text))
             for fnd in set(found):
-                searched.add(SuspiciousCode(fnd, _type, lvl))
-        return tuple(searched)
+                searched.add(SuspiciousCode(fnd, pc.danger_lvl, pc.type))
+        return list(searched)
 
-    def _get_next_pattern(self) -> Iterator[(re.Pattern, DangerLevel, SuspiciousType)]:
-        pass
-
-
-PATTERNS = {
-    Language.GENERAL: {
-        DangerLevel.SUSPICIOUS: (
-            re.compile(br'\W([\"\']/bin/sh[\"\'])\W', re.IGNORECASE),
-            re.compile(br'((?:web)?shell(?:exec)?)', re.IGNORECASE),
-        ),
-        DangerLevel.PAY_ATTENTION: (
-            re.compile(br'[_\W](base_?(?:16|32|64|85))', re.IGNORECASE),
-            re.compile(br'[_\W](rot_?13)', re.IGNORECASE),
-        )
-    },
-    Language.PHP: {
-        DangerLevel.DANGEROUS: (
-            re.compile(br'[^\w\'\"]([Ee]val)(?:\s+[\'\"\w]|\s*\()'),
-
-            re.compile(br'\W(passthru)\s*\('),
-            re.compile(br'\W(proc_open)\s*\('),
-            re.compile(br'[^\w\'\"](system)(?:\s+[\'\"\w]|\s*\()'),
-        ),
-        DangerLevel.SUSPICIOUS: (
-            re.compile(br'\W(include(?:_once)?\s*\(.+?\))'),
-
-            re.compile(br'\W(gzinflate)\s*\('),
-            re.compile(br'\W(base64_(?:en|de)code)\s*\('),
-            re.compile(br'\W(convert_uu(?:en|de)code)\s*\('),
-            re.compile(br'\W(str_rot13)\s*\('),
-
-            re.compile(br'\W([lf]?ch(?:grp|mod|own)(?:Sync|))\s*\('),
-            re.compile(br'\W(file(?:_exists|_get_contents|_put_contents|inode|size|type|\?))\s*\('),
-            re.compile(br'\W(f(?:cntl|open|passthru|read|scanf|[lr]?seek|(?:data)?sync|stat|write))\s*\('),
-            re.compile(br'\W(dirname)\s*\('),
-            re.compile(br'\W(is_?(?:dir|executable|file|link|readable|uploaded_file|write?able))\s*\('),
-            re.compile(br'\W(mk(?:dirs?|node)(?:Sync|))\s*\('),
-            re.compile(br'\W(move_uploaded_file)\s*\('),
-            re.compile(br'\W(pathinfo)\s*\('),
-            re.compile(br'\W(p(?:open|readv?|writev?|ipe))\s*\('),
-            re.compile(br'\W(rm(?:dir|)(?:Sync|))\s*\('),
-
-            re.compile(
-                br'\W(eio_(?:f?ch(?:mod|own)|fallocate|'
-                + br'ftruncate|init|link|mk(?:dir|nod)|open|'
-                + br'read(?:dir|link)?|realpath|rmdir|'
-                + br'seek|sendfile|write))\s*\('
-            ),
-
-            re.compile(br'\W(\$_FILES(?:\s*\[.+?])+)'),
-        ),
-        DangerLevel.PAY_ATTENTION: (
-            re.compile(br'\W(require(?:_once)?\s*\(.+?\))'),
-
-            re.compile(br'\W(\$_GET\s*\[.+?])'),
-            re.compile(br'\W(\$_REQUEST\s*\[.+?])'),
-        )
-    },
-    Language.JS: {
-        DangerLevel.DANGEROUS: (
-            re.compile(br'\W(document\s*\.\s*write)\s*\('),
-            re.compile(br'\W(innerHTML)\s*\('),
-
-            re.compile(br'[^\w\'\"]((?:shell_|pcntl_)?exec(?:File|[vl]p?e?|)(?:Sync|))(?:\s+[\'\"\w]|\s*\()'),
-        ),
-        DangerLevel.SUSPICIOUS: (
-            re.compile(br'\W(atob)\s*\('),
-            re.compile(br'\W(btoa)\s*\('),
-
-            re.compile(br'\W(require\s*\(\s*[\"\'](?:fs|multer|express-fileupload|socket\.io)[\"\']\s*\))'),
-
-            re.compile(br'\W(appendFile(?:Sync|))\s*\('),
-            re.compile(br'\W(create(?:Read|Write)Stream)\s*\('),
-            re.compile(br'\.(fd)\W'),
-            re.compile(br'\W(read(?:v|dir|[Ff]ile|link|[Ll]ines?|partial|ableWebStream)(?:Sync|))\s*\('),
-            re.compile(br'\W(write(?:v|File)(?:Sync|))\s*\('),
-            re.compile(br'\W(open(?:dir|AsBlob)(?:Sync|))\s*\('),
-        )
-    },
-    Language.PYTHON: {
-        DangerLevel.SUSPICIOUS: (
-            re.compile(br'\W((?:standard_|urlsafe_)?[ab](?:16|32|64|85)(?:hex)?(?:en|de)code)\s*\('),
-
-            re.compile(br'\W(access)\s*\('),
-            re.compile(br'\.(environb?)\W'),
-            re.compile(br'\W(get(?:envb?|_exec_path))\s*\('),
-            re.compile(br'\W(putenv)\s*\('),
-            re.compile(br'\W(removedirs)\s*\('),
-        ),
-        DangerLevel.PAY_ATTENTION: (
-            re.compile(br'(with\s+open\(.+?\)\s+as.+?:)'),
-            re.compile(br'(=\s*open\(.+\))'),
-
-            re.compile(br'\W(urllib\.urlretrieve)\s*\('),
-            re.compile(br'\W(requests\.get)\s*\('),
-        )
-    },
-
-    Language.RUBY: {
-        DangerLevel.SUSPICIOUS: (
-            re.compile(br'\W((?:strict_|urlsafe_)?(?:en|de)code64)\s*[({]'),
-
-            re.compile(br'\W((?:re|sys)open)\s*\('),
-            re.compile(br'\W(each_line)\s*[({]'),
-            re.compile(br'\Wf(?:ile)?\s*\.\s*((?:write|read)(?:lines?)?)', re.IGNORECASE),
-
-            re.compile(br'\W(directory\?)\s*\('),
-            re.compile(br'\W((?:read|execut)able(?:_real)?\?)\s*\('),
-        ),
-        DangerLevel.PAY_ATTENTION: (
-            re.compile(br'\W(Net\s*::\s*HTTP\s*\.\s*get(?:_response)?\s*\(.+\))'),
-            re.compile(br'\W(http\s*\.\s*request(?:_get)?\s*\(.+\))'),
-            re.compile(br'\W((?:popen|capture)(?:3|2e?))\s*\('),
-        )
-    },
-    Language.C_SHARP: {
-        DangerLevel.SUSPICIOUS: (
-            re.compile(br'\W(FileStream)\W'),
-            re.compile(br'\W(ICodeCompiler)\W'),
-            re.compile(br'\W(CodeDom\.Compiler[.;])\W'),
-            re.compile(br'\W(CodeAnalysis\.(?:CSharp\.)?Scripting[.;])\W'),
-            re.compile(br'\W(CSharpScript\.(?:Run|Evaluate)Async)\W'),
-
-            re.compile(br'\W(Download(?:Data|File|String)(?:(?:Task)?Async)?)\s*\('),
-            re.compile(br'\W(FileMode\s*.\s*OpenOrCreate)\W'),
-
-            re.compile(br'\W(DriveInfo)\W'),
-            re.compile(br'\W(DirectoryInfo)\W'),
-            re.compile(br'\W(Create(?:Subd|D)irectory|SymbolicLink)\s*\('),
-            re.compile(br'\W(Enumerate(?:File(?:System(?:Infos)?|s)|Directories))\s*\('),
-            re.compile(
-                br'\W(Get(?:Drives|File(?:SystemEntries|s|Name|)|(?:Current)?Directory(?:Name)?|Directories))\s*\('
-            ),
-            re.compile(br'\W(Open(?:Read|Text|Write))\s*\('),
-            re.compile(br'\W((?:Read|Write)All(?:Lines|Bytes|Text)(?:Async)?)\s*\('),
-            re.compile(br'\W(ReadLines(?:Async)?)\s*\('),
-
-            re.compile(br'\W((?:From|To)Base64(?:Transform|String|CharArray))\W'),
-            re.compile(br'\W(SoapHexBinary)\W'),
-        )
-    }
-}
+    def _get_next_pattern(self) -> Iterator[PatternContainer]:
+        for lvl in self._patterns:
+            for type_ in self._patterns[lvl]:
+                for pat in self._patterns[lvl][type_]:
+                    yield PatternContainer(pat, lvl, type_)
 
 
 def main():
-    text = read_file('../../source/x.txt')
-    # text = input().encode()
-    pat_set = set()
-    for lvl in DangerLevel:
-        print(f'\n{"-" * (8 + len(lvl.name))}\n\t{lvl.name}\n{"-" * (8 + len(lvl.name))}')
-        for lang in Language:
-            if not (lang in PATTERNS and lvl in PATTERNS[lang]):
-                continue
-            for pat in PATTERNS[lang][lvl]:
-                if pat in pat_set:
-                    continue
-                pat_set.add(pat)
-                found = set(pat.findall(text))
-                if found:
-                    print(f'\n{pat}')
-                    for fnd in found:
-                        print(f'\t{fnd}')
+    # text = read_file('../../source/x.txt')
+    text = input().encode()
+    searched = SuspySearcher().search(text)
+    for sr in searched:
+        print(sr)
 
 
 if __name__ == '__main__':
