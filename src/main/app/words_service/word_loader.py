f4bc091292e3e307e373273c27ffe35a5c6ae60f
arcibashev71 <arcibashev71@yandex.ru>
2024-04-24 22:51:32 +0500

    ref: добавить комментарии к WordLoader

--- src/main/app/words_service/word_loader.py
+++ src/main/app/words_service/word_loader.py
@@ -4,19 +4,36 @@ from src.main.app.file_reader import read_file_by_pickle
 
 
 class WordLoader:
+    """Загружает словарь слов в память"""
+
     def __init__(self, path: str = None):
         self._path = path
 
     @property
     def path(self) -> str:
+        """
+        :return: путь до словаря слов
+        """
         return self._path
 
     @path.setter
     def path(self, new: str) -> None:
+        """
+        Устанавливает путь до словаря слов
+
+        :param new: новый путь до словаря слов
+
+        :return: None
+        """
         self._path = new
 
     @abstractmethod
     def load(self) -> dict[int, set[tuple[int]]]:
+        """
+        Загружает словарь слов (по пути path) в память в виде dict[длина слова] = {слова}
+
+        :return: загруженный словарь
+        """
         pass
 
 
