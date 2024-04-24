d4edd7c4ccfc39b53962794695d658231d4a7658
arcibashev71 <arcibashev71@yandex.ru>
2024-04-24 22:08:52 +0500

    test: дописать тесты на WordExtractor

--- src/test/test_extractors/test_word_extractor.py
+++ src/test/test_extractors/test_word_extractor.py
@@ -8,7 +8,7 @@ class TestWordExtractor(unittest.TestCase):
     def setUp(self) -> None:
         self._extractor = WordExtractor()
 
-    def test__is_new_lexeme(self):
+    def test__is_new_word(self):
         strings_false = [
             (None, b'a'),
             (None, b'A'),
@@ -28,36 +28,64 @@ class TestWordExtractor(unittest.TestCase):
 
     def test_one_letter1(self):
         string = tuple(b'a')
-        expected = Word(string)
+        expected = expected_low = Word(string)
+
+        actual_low = list(self._extractor.get_word_iter(string, to_lower=True))
+        self.assertEqual(expected_low, *actual_low)
+
         actual = list(self._extractor.get_word_iter(string, to_lower=False))
         self.assertEqual(expected, *actual)
 
     def test_one_letter2(self):
         string = tuple(b'A')
+
         expected = Word(string)
         actual = list(self._extractor.get_word_iter(string, to_lower=False))
         self.assertEqual(expected, *actual)
 
+        expected_low = Word(tuple(b'a'))
+        actual_low = list(self._extractor.get_word_iter(string, to_lower=True))
+        self.assertEqual(expected_low, *actual_low)
+
     def test_camel_case1(self):
         string = tuple(b'first')
-        expected = Word(string)
+        expected = expected_low = Word(string)
+
+        actual_low = list(self._extractor.get_word_iter(string, to_lower=True))
+        self.assertEqual(expected_low, *actual_low)
+
         actual = list(self._extractor.get_word_iter(string, to_lower=False))
         self.assertEqual(expected, *actual)
 
     def test_camel_case2(self):
         string = tuple(b'firstSecond')
+
+        expected_low = list(map(self._map_str_to_word, ['first', 'second']))
+        actual_low = list(self._extractor.get_word_iter(string, to_lower=True))
+        self.assertEqual(expected_low, actual_low)
+
         expected = list(map(self._map_str_to_word, ['first', 'Second']))
         actual = list(self._extractor.get_word_iter(string, to_lower=False))
         self.assertEqual(expected, actual)
 
     def test_pascal_case1(self):
         string = tuple(b'First')
-        expected = self._map_str_to_word('First')
+
+        expected_low = self._map_str_to_word('first')
+        actual_low = list(self._extractor.get_word_iter(string, to_lower=True))
+        self.assertEqual(expected_low, *actual_low)
+
         actual = list(self._extractor.get_word_iter(string, to_lower=False))
+        expected = self._map_str_to_word('First')
         self.assertEqual(expected, *actual)
 
     def test_pascal_case2(self):
         string = tuple(b'FirstSecond')
+
+        expected_low = list(map(self._map_str_to_word, ['first', 'second']))
+        actual_low = list(self._extractor.get_word_iter(string, to_lower=True))
+        self.assertEqual(expected_low, actual_low)
+
         expected = list(map(self._map_str_to_word, ['First', 'Second']))
         actual = list(self._extractor.get_word_iter(string, to_lower=False))
         self.assertEqual(expected, actual)
@@ -65,11 +93,17 @@ class TestWordExtractor(unittest.TestCase):
     def test_one_upper_word(self):
         string = tuple(b'FIRST')
         expected = self._map_str_to_word('FIRST')
+        actual_low = list(self._extractor.get_word_iter(string, to_lower=True))
         actual = list(self._extractor.get_word_iter(string, to_lower=False))
         self.assertEqual(expected, *actual)
 
     def test_small_and_upper_words(self):
         string = tuple(b'firstSECOND')
+
+        expected_low = list(map(self._map_str_to_word, ['first', 'second']))
+        actual_low = list(self._extractor.get_word_iter(string, to_lower=True))
+        self.assertEqual(expected_low, actual_low)
+
         expected = list(map(self._map_str_to_word, ['first', 'SECOND']))
         actual = list(self._extractor.get_word_iter(string, to_lower=False))
         self.assertEqual(expected, actual)
