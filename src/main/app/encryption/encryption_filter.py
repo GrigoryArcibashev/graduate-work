d368767ebba374718a3316c59afc52653eb32987
arcibashev71 <arcibashev71@yandex.ru>
2024-04-28 20:00:07 +0500

    ref(comments)

--- src/main/app/encryption/encryption_filter.py
+++ src/main/app/encryption/encryption_filter.py
@@ -3,22 +3,12 @@ from src.main.app.extractors.token import TokenType, Token
 from src.main.app.extractors.token_extractor import TokenExtractor
 from src.main.app.extractors.word_extractor import WordExtractor
 
-"""
-ШИФР ШИФР
-    включаем все
-ШИФР НЕ_ШИФР
-    включаем вторую половину
-НЕ_ШИФР ШИФР
-    включаем первую половину
-НЕ_ШИФР НЕ_ШИФР
-    удаляем все
-
-если процент распознанных символов >50% => НЕ_ШИФР
-иначе ШИФР
-"""
-
 
 class EncryptionFilter:
+    """
+    Находит и удаляет незашифрованную часть текста
+    """
+
     def __init__(self, word_dict_service: WordDictService):
         self._word_dict_service = word_dict_service
         self._token_extractor = TokenExtractor()
@@ -26,6 +16,12 @@ class EncryptionFilter:
         self._encryption_boundary = 0.5
 
     def filter(self, data: list[int]) -> list[int]:
+        """
+        Фильтрует текст: находит и удаляет незашифрованную часть текста
+
+        :param data: текст в виде последовательности номеров байт
+        :return: "отфильтрованный" текст в виде последовательности номеров байт
+        """
         result = list()
         prev_let_token_is_encr = True
         current_non_letter_tokens = list()
@@ -43,7 +39,27 @@ class EncryptionFilter:
         result.extend(current_non_letter_tokens)
         return self.map_tokens_to_bytes(result)
 
-    def process_letter_token(self, token, current_non_letter_tokens, prev_let_token_is_encr) -> (list[Token], bool):
+    def process_letter_token(
+            self,
+            token: Token,
+            current_non_letter_tokens: list[Token],
+            prev_let_token_is_encr: bool
+    ) -> (list[Token], bool):
+        """
+        Основываясь на ранее считанные символы и значение очередного
+        буквенного токена определяет, какие символы будут сохранены,
+        а какие вырезаны
+
+        :param token: очередной буквенный токен (TokenType.LETTERS)
+        :param current_non_letter_tokens: все небуквенные токены
+        между последним буквенным и текущим
+
+        :param prev_let_token_is_encr: было ли значение последнего
+        токена шифром (да-True, нет-False)
+        :return: все сохраненные символы из current_non_letter_tokens
+        между буквенными токенами и сам токен, если его значение
+        было признано зашифрованным
+        """
         res = list()
         if self.is_letter_token_encr(token):
             if prev_let_token_is_encr:  # encr _ encr
@@ -60,7 +76,13 @@ class EncryptionFilter:
             is_encr = False
         return res, is_encr
 
-    def is_letter_token_encr(self, token):
+    def is_letter_token_encr(self, token: Token) -> bool:
+        """
+        Определяет, является ли значение буквенного токена зашифрованным
+
+        :param token: буквенный токен (TokenType.LETTERS)
+        :return: True, если значение зашифровано, иначе - False
+        """
         good_count = 0
         for word in self._word_extractor.get_word_iter(token.value):
             if self._word_dict_service.check_word(word):
@@ -69,6 +91,13 @@ class EncryptionFilter:
 
     @staticmethod
     def map_tokens_to_bytes(tokens: list[Token]) -> list[int]:
+        """
+        Переводит последовательность токенов в последовательность их значений в виде номеров байт
+
+        :param tokens: последовательность токенов
+
+        :return: последовательность значений токенов в виде номеров байт
+        """
         result = list()
         for token in tokens:
             result.extend(token.value)
