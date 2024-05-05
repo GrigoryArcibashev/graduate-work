b83a50aa4d5d7732fa907acec4b164d7169c784d
arcibashev71 <arcibashev71@yandex.ru>
2024-05-05 13:18:52 +0500

    feat: read_json

--- src/main/app/util/file_reader.py
+++ src/main/app/util/file_reader.py
@@ -1,3 +1,4 @@
+import json
 import pickle
 from typing import Any
 
@@ -10,3 +11,8 @@ def read_file(path: str) -> bytes:
 def read_file_by_pickle(path: str) -> Any:
     with open(path, 'rb') as f:
         return pickle.load(f)
+
+
+def read_json(path: str) -> Any:
+    with open(path, 'r') as f:
+        return json.load(f)
