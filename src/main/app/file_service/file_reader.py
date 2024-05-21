467f76a1b948194715ec6ca6b23f6e836a275646
arcibashev71 <arcibashev71@yandex.ru>
2024-05-21 16:52:07 +0500

    feat: реализовать чтение файла поблочно

--- src/main/app/file_service/file_reader.py
+++ src/main/app/file_service/file_reader.py
@@ -1,9 +1,17 @@
 import json
 import pickle
-from typing import Any
+from typing import Any, Iterator
 
 
 class FileReader:
+    @staticmethod
+    def read_file_by_iter(path: str, buffer_size: int) -> Iterator[bytes]:
+        with open(path, 'rb') as f:
+            read = f.read(buffer_size)
+            while read:
+                yield read
+                read = f.read(buffer_size)
+
     @staticmethod
     def read_file(path: str) -> bytes:
         with open(path, 'rb') as f:
